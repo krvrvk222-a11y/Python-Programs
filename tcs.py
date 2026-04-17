54180327463c5760d7addd8c0a0f067f9d80cc2e
KRV KARTHIK <krvrvk222@gmail.com>
2026-04-17 23:20:02 +0530

    feature added

--- tcs.py
+++ tcs.py
@@ -1,16 +1,11 @@
-# Non-Repeating Element
+# This are the most asked TCS coding questions.
 
-# Find the first non-repeating element in a given array arr of integers and if there is not present any non-repeating element then return 0
-# Note: The array consists of only positive and negative integers and not zero.
-# Examples:
-# Input: arr[] = [-1, 2, -1, 3, 2]
-# Output: 3
-# Explanation: -1 and 2 are repeating whereas 3 is the only number occuring once. Hence, the output is 3. 
-# Input: arr[] = [1, 1, 1]
-# Output: 0
-# Explanation: There is not present any non-repeating element so answer should be 0.
-# Expected Time Complexity: O(n).
-# Expected Auxiliary Space: O(n).
+
+
+
+
+
+# Non-Repeating Element
 
 #PROGRAM ==>
 
